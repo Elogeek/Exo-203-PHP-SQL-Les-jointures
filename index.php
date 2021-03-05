6dcac76d604aa0fe2e263a433b8c56f37f1c2e41
elogeek <elodiechristin@gmail.com>
2021-03-05 15:39:50 +0100

    start exo

--- index.php
+++ index.php
@@ -1,8 +1,54 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <canvas id="chart">
+        <canvas id="myChart"></canvas>
+    </canvas>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.css" integrity="sha512-C7hOmCgGzihKXzyPU/z4nv97W0d9bv4ALuuEbSf6hm93myico9qa0hv4dODThvCsqQUmKmLcJmlpRmCaApr83g==" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.css" integrity="sha512-/zs32ZEJh+/EO2N1b0PEdoA10JkdC3zJ8L5FTiQu82LR9S/rOQNfQN7U59U9BC12swNeRAz3HSzIL2vpp4fv3w==" crossorigin="anonymous" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js" integrity="sha512-hZf9Qhp3rlDJBvAKvmiG+goaaKRZA6LKUO35oK6EsM0/kjPK32Yw7URqrq3Q+Nvbbt8Usss+IekL7CRn83dYmw==" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.bundle.min.js" integrity="sha512-SuxO9djzjML6b9w9/I07IWnLnQhgyYVSpHZx0JV97kGBfTIsUYlWflyuW4ypnvhBrslz1yJ3R+S14fdCWmSmSA==" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.bundle.js" integrity="sha512-zO8oeHCxetPn1Hd9PdDleg5Tw1bAaP0YmNvPY8CwcRyUk7d7/+nyElmFrB6f7vg4f7Fv4sui1mcep8RIEShczg==" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js" integrity="sha512-d9xgZrVZpmmQlfonhQUvTR7lMPtO7NkZMkA0ABN3PHCbKA5nqylQ/yWlFAyY6hYgdF1Qh6nYiuADWwKB4C2WSw==" crossorigin="anonymous"></script>
+    <script src="script.js"></script>
+</body>
+</html>
 <?php
 
 $servername = 'localhost';
-$username = '';
-$password = '';
-$dbname = '';
+$username = 'root';
+$password = 'dev';
+$dbname = 'Classes';
 
+try {
+    $connect =new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
+    $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $connect->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+    $sql = $connect->prepare("INSERT INTO eleve(prenom, nom, login, password, information_id) 
+                                    VALUES ('Sara', 'Doe', 'doe@gmail.com', 'azerty', 1),
+                                           ('John', 'Doe', 'Jdoe@gmail.com', 'bouh', 2),
+                                           ('Jane', 'Doe', 'dJAoe@gmail.com', 'dRZS', 3)");
+
+    $sql = $connect->prepare("SELECT eleve.prenom,eleve.nom,eleve.information_id
+                                    FROM eleve
+                                      INNER JOIN eleve_competence ec on eleve.id = ec.eleve_id
+                                      INNER JOIN eleve_information ei on eleve.information_id = ei.id
+                                      INNER JOIN competence c on ec.competence_id = c.id
+                              ");
+
+    $sql->execute();
+}
+catch(PDOException $exeption) {
+    echo 'error'. $exeption->getMessage();
+}
 ?>
